d5aef5c3f039d4f984e3d63fa29d6a6b3629d967
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-28 17:54:05 +0700

    Intial commit

--- flag.asm
+++ flag.asm
@@ -87,8 +87,9 @@ tea_t002:
 	mov rax, [rdi + 8]
 	ror rax, 14
 	mov rdx, [rsi + 8]
-	and rax, 0xffffffff00000000
-	and rdx, 0xffffffff00000000
+	mov rcx, 0xffffffff00000000
+	and rax, rcx
+	and rdx, rcx
 	cmp rax, rdx
 	jne .L0
 
